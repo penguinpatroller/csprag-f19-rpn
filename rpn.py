b799b8ce29042e079d10cd3238b2cbfa7ba3b57f
penguinpatroller <camdav@umich.edu>
2019-11-06 14:36:25 -0500

    Added power function to rpn calculator. Currently, I am not the king of python, so we will see if this works. I only added one line anyway, I mean how bad could it be.

--- rpn.py
+++ rpn.py
@@ -1,29 +1,37 @@
-#!/user/bin/env python3
+#!/usr/bin/env python3
 
-def calculate(arg):
-	stack = list()
-	for token in arg.split():
-		if token == '+':
-			arg2 = stack.pop()
-			arg1 = stack.pop()
-			result = arg1 + arg2
-			stack.append(result)
-		elif token == '-':
-                         arg2 = stack.pop()
-                         arg1 = stack.pop()
-                         result = arg1 - arg2
-                         stack.append(result)
-		else:
-			stack.append(int(token))
-		#print(stack)
-	if len(stack) != 1:
-		raise TypeError('malinformed input')
-	return stack.pop()
+import operator
+
+
+operators = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.truediv,
+	'^': operator.pow,
+}
+
+def calculate(myarg):
+    stack = list()
+    for token in myarg.split():
+        try:
+            token = int(token)
+            stack.append(token)
+        except ValueError:
+            function = operators[token]
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            result = function(arg1, arg2)
+            stack.append(result)
+        print(stack)
+    if len(stack) != 1:
+        raise TypeError("Too many parameters")
+    return stack.pop()
 
 def main():
-	while True:
-		calculate(input("rpn calc> "))
+    while True:
+        result = calculate(input("rpn calc> "))
+        print("Result: ", result)
 
 if __name__ == '__main__':
-	main()
-
+    main()
